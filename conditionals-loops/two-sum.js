001995edf699384f294c470e81a8cb64e3f51dc7
pushpendu <pushpendu.ghosh@i2itherapy.com>
2023-08-19 12:34:44 +0530

    two sum problem using loops

--- conditionals-loops/two-sum.js
+++ conditionals-loops/two-sum.js
@@ -1,4 +1,3 @@
-
 // https://leetcode.com/problems/two-sum/
 
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -7,10 +6,26 @@
 
 // You can return the answer in any order.
 
- 
-
 // Example 1:
 
-// Input: nums = [2,7,11,15], target = 9
+// Input: nums = [2,7,11,15, 8, 1] target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+var twoSum = function (nums, target) {
+
+  for (let i = 0; i < nums.length; i++) {
+
+      console.log(i , "this is i");
+    for (let j = i + 1; j < nums.length; j++) {
+
+       console.log(j , "this is j");
+      if (nums[i] + nums[j] === target) {
+        console.log([i, j]);
+      }
+    }
+  }
+
+};
+
+twoSum([2, 7, 11, 15, 8, 1], 9);
